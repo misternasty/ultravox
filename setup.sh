4d0d3be0be241fc17adf9540a272a9a44e67f8a2
Doug MrDoug <151004437+misternasty@users.noreply.github.com>
2025-02-20 10:14:10 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
-cd $HOME
+cd /root
 mkdir workspace
 cd workspace
-git clone https://github.com/fixie-ai/ultravox.git -b main
+git clone https://github.com/misternasty/ultravox.git -b main
 cd ultravox
-mkdir -p ~/.local/bin
+mkdir -p local/bin
 curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to ~/.local/bin
 just install
-bash ./scripts/vscode_tunnel.sh
