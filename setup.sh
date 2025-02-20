e67ef52ea12f433b4aaf6eb392cd2398536b601e
Doug MrDoug <151004437+misternasty@users.noreply.github.com>
2025-02-20 10:27:29 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ cd workspace
 git clone https://github.com/misternasty/ultravox.git -b main
 cd ultravox
 mkdir -p local/bin
-curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to ~/.local/bin
+curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to local/bin
 just install
